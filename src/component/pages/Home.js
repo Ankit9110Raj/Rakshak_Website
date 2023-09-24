28f16bdbb8d5b74ef5a13b3cdbde67e1c5eb85b7
Ankit Raj <ankitraj@Ankits-MacBook-Air.local>
2023-09-24 21:49:15 +0530

    pages added

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -39,6 +39,32 @@ const Home = () =>{
                 }
 
     </Carousel>
+    <div className="image-container">
+      <img
+        src="https://rakshak-iitb.org/assets/images/logo4.gif"
+        alt="Example"
+        className="responsive-image-home"
+      />
+    </div>
+    <div className="paragraph-container">
+        <p className="responsive-paragraph">Rakshak IITB is the official team of IIT Bombay consisting of 40+ students from various disciplines who work collaboratively to develop a fleet of robust Unmanned Aerial Vehicles (UAVs) to support Search and Rescue Operations (SRO) in the event of a disaster. The team was formed in the year 2015 by some of the IITB enthusiasts to build drones/planes that can be used for rescue missions. These aircraft can provide relief measures in the disaster-stricken area (e.g., Landslide, Flood, Earthquake) and can be used for surveys & mapping of cities and conservation of wildlife with slight modifications. Our design efficiently tackles the above problems cost-effectively without compromising on human safety.
+
+
+
+        As of now, we have developed five planes. Each year we are trying to stretch our limits by our innovation and teamwork. We would also like to thank IITB for supporting us and providing the platform to showcase our talent at the international level.</p>
+    </div>
+
+    <div className="video-container">
+      <iframe
+        title="YouTube Video"
+        src="https://www.youtube.com/embed/wG5wcbJa85s"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+        className="responsive-iframe"
+      ></iframe>
+    </div>
+
     </>
   )
 }
--- src/component/pages/Planes.js
+++ src/component/pages/Planes.js
@@ -1,8 +1,80 @@
 import React from 'react'
+import "./Planes.css"
 
 const Planes = () => {
   return (
-    <div>Planes</div>
+    <>
+    <div className='ourPlanes'>OUR PLANES</div>
+    <div className='heading'>SKYWALKER</div>
+    <div className="container">
+      <div className="image">
+        <img
+          src="https://rakshak-iitb.org/assets/images/skywalker-800x533.jpg"
+          alt="Image"
+          className="responsive-image"
+        />
+      </div>
+      <div className="text">
+        <p>Built in 2017, a recent addition to the Team's plane collection. Designed for the AUVSI-SUAS competition, the plane has an endurance of 30-40 minutes of completely autonomous operation of object detection and classification, interoperability, waypoint navigation, flight, take-off and landing. It has a take-off gross weight of 7.5Kgs with a cruise speed of 12 m/s.</p>
+      </div>
+    </div>
+
+    <div className='heading'>SKYWALKER</div>
+   <div className="container">
+   <div className="text-even">
+        <p>Built in 2017, a recent addition to the Team's plane collection. Designed for the AUVSI-SUAS competition, the plane has an endurance of 30-40 minutes of completely autonomous operation of object detection and classification, interoperability, waypoint navigation, flight, take-off and landing. It has a take-off gross weight of 7.5Kgs with a cruise speed of 12 m/s.</p>
+      </div>
+      <div className="image">
+        <img
+          src="https://rakshak-iitb.org/assets/images/skywalker-800x533.jpg"
+          alt="Image"
+          className="responsive-image"
+        />
+      </div>  
+    </div>
+
+    <div className='heading'>SKYWALKER</div>
+    <div className="container">
+      <div className="image">
+        <img
+          src="https://rakshak-iitb.org/assets/images/skywalker-800x533.jpg"
+          alt="Image"
+          className="responsive-image"
+        />
+      </div>
+      <div className="text">
+        <p>Built in 2017, a recent addition to the Team's plane collection. Designed for the AUVSI-SUAS competition, the plane has an endurance of 30-40 minutes of completely autonomous operation of object detection and classification, interoperability, waypoint navigation, flight, take-off and landing. It has a take-off gross weight of 7.5Kgs with a cruise speed of 12 m/s.</p>
+      </div>
+    </div>
+
+    <div className='heading'>SKYWALKER</div>
+   <div className="container">
+   <div className="text-even">
+        <p>Built in 2017, a recent addition to the Team's plane collection. Designed for the AUVSI-SUAS competition, the plane has an endurance of 30-40 minutes of completely autonomous operation of object detection and classification, interoperability, waypoint navigation, flight, take-off and landing. It has a take-off gross weight of 7.5Kgs with a cruise speed of 12 m/s.</p>
+      </div>
+      <div className="image">
+        <img
+          src="https://rakshak-iitb.org/assets/images/skywalker-800x533.jpg"
+          alt="Image"
+          className="responsive-image"
+        />
+      </div>  
+    </div>
+    
+    <div className='heading'>SKYWALKER</div>
+    <div className="container">
+      <div className="image">
+        <img
+          src="https://rakshak-iitb.org/assets/images/skywalker-800x533.jpg"
+          alt="Image"
+          className="responsive-image"
+        />
+      </div>
+      <div className="text">
+        <p>Built in 2017, a recent addition to the Team's plane collection. Designed for the AUVSI-SUAS competition, the plane has an endurance of 30-40 minutes of completely autonomous operation of object detection and classification, interoperability, waypoint navigation, flight, take-off and landing. It has a take-off gross weight of 7.5Kgs with a cruise speed of 12 m/s.</p>
+      </div>
+    </div>
+    </>
   )
 }
 
